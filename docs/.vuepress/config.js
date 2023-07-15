f532f84aa8e74667da0eec6425dd4c14555db7bf
Jomsy98 <james@salvia.me>
2023-07-15 15:53:06 -0700

    Updated home page with basic information

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebar = require("./sidebar.js");
 export default {
   title: "SWMG",
   // Appears in meta tag and subtitle
-  description: "DnD Discord Bot",
+  description: "A Discord bot to enhance your online D&D experience. ",
 
   theme: defaultTheme({
     // Links that will appear in the top navbar
